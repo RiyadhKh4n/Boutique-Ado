fd3e61354b1ac630cf3be1d0448e61d42380adda
Arkay <riyadh.k@outlook.com>
2022-05-10 16:09:39 +0000

    Add checkout model

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home', 'products', 'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
